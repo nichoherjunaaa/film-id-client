1d69a6f4ba4952f9eb87ead8e3e34a45dc5118d8
nichoherjunaa <nichoherjunaa@gmail.com>
2025-04-12 23:15:46 +0700

    detail card fix

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const CardDetail = ({ image, title, description, popularity }) => {
     return (
         <>
-            <div className="card shadow-lg bg-base-100 w-5xl">
+            <div className="card shadow-lg bg-base-100 max-w-4xl">
                 <div className="flex">
-                    <figure className="w-full aspect-[2/3] overflow-hidden">
+                    <figure className="w-300 h-160 overflow-hidden">
                         <img
                             src={`https://image.tmdb.org/t/p/w500${image}`}
                             alt={title}
@@ -14,7 +14,7 @@ const CardDetail = ({ image, title, description, popularity }) => {
                     </figure>
                     <div className="card-body items-center">
                         <h2 className="card-title text-center">{title}</h2>
-                        <p>{description}</p>
+                        <p className="line-clamp-3">{description}</p>
                         <button className="btn btn-primary w-full">Buy</button>
                     </div>
                 </div>
